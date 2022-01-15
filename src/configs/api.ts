08746ca425362017aca7402d4887b1e183627426
19750648 <idgrishin@sberbank.ru>
2022-01-15 14:03:43 +0300

    basic setup

---
+++ src/configs/api.ts
@@ -0,0 +1,11 @@
+/**
+ Основной адрес футбол даты
+ */
+export const API_MAIN = 'https://www.football-data.org'
+
+/**
+ ключ апи из окружения
+ */
+export const API_KEY = process.env.REACT_APP_API_KEY
+
+
